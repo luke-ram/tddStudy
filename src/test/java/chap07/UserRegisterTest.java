3389fdef166a0f685b40fc23d0c3bb53ab344174
luke-ram <ramhosoo@gmail.com>
2021-02-27 11:46:46 +0900

    chap07 test code setup

---
+++ src/test/java/chap07/UserRegisterTest.java
@@ -0,0 +1,27 @@
+package chap07;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UserRegisterTest {
+    private UserRegister userRegister;
+    private StubWeakPasswordChecker stubPasswordChecker = new StubWeakPasswordChecker();
+
+    @BeforeEach
+    void setUp(){
+        userRegister = new UserRegister(stubPasswordChecker);
+    }
+
+    @DisplayName("약한 암호면 가입 실패")
+    @Test
+    void weakPassword(){
+        stubPasswordChecker.setWeak(true);
+
+        assertThrows(WeakPasswordException.class, () -> {
+            userRegister.register("id","pw","email");
+        });
+    }
+}
